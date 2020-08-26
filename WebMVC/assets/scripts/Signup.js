2b0fbe9f8795e40309178adc8fb46ca7cca9f760
lushan1980 <shanswork@yahoo.com>
2020-08-26 19:32:24 -0400

    commit8/26

--- WebMVC/assets/scripts/Signup.js
+++ WebMVC/assets/scripts/Signup.js
@@ -65,4 +65,14 @@
         if (!results[2]) return '';
         return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
+
+    $(".toggle-password").click(function () {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
 })
---
+++ WebMVC/assets/scripts/VASSurvey2.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    for (var i = 18; i <= 80; i++) {
+        var select = document.getElementById("age");
+        var option = document.createElement("OPTION");
+        select.options.add(option);
+        option.text = i;
+        option.value = i;
+    }
+    
+    function displayRadioValue(name) {
+        var ele = document.getElementsByName(name);
+        for (i = 0; i < ele.length; i++) {
+            if (ele[i].checked)
+                var val = ele[i].value;
+        }
+        return val;
+    } 
+
+    var gender = displayRadioValue(document.getElementsByName('gender')),
+        Ethnicity = displayRadioValue(document.getElementsByName('Ethnicity')),
+        Race = displayRadioValue(document.getElementsByName('Race'))
+
+
+
+
+})
